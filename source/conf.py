a2ad3f106301d31f5772e5db7e8045764f2049e4
nepcore <nepcore@griem.xyz>
2021-05-12 17:09:53 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -5,7 +5,7 @@ author = u''
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'2.x'
+release = u'1.x'
 
 
 # If your documentation needs a minimal Sphinx version, state it here.
