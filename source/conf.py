35412c30013291f13503aa39d4537f58f36e01f3
Joshua Taylor <lordralex@gmail.com>
2024-11-26 13:20:48 -0600

    Attempt to fix 404

--- source/conf.py
+++ source/conf.py
@@ -12,7 +12,7 @@ release = u'3.x'
 #
 # needs_sphinx = '1.0'
 
-extensions = ['sphinx_inline_tabs', 'sphinx_copybutton']
+extensions = ['sphinx_inline_tabs', 'sphinx_copybutton', 'notfound.extension']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -118,3 +118,8 @@ epub_title = project
 # epub_uid = ''
 
 epub_exclude_files = ['search.html']
+
+notfound_context = {
+    'title': 'Fish not found',
+    'body': "<h1>Page not found</h1>\n\nUnfortunately we couldn't find the content you were looking for.",
+}
